8770e515acc8a263b1f423308fce09467aabb790
Bysenk0 <solisske@gmail.com>
2024-02-22 15:37:32 -0600

    add new dates for register

---
+++ database/migrations/2024_02_22_212903_add_columns_to_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       Schema::table('users', function (Blueprint $table) {
+        $table->string('last_name_1')->after('name')->nullable();
+        $table->string('last_name_2')->after('last_name_1')->nullable();
+        $table->unsignedBigInteger('gender_id')->nullable();
+        $table->unsignedTinyInteger('status')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+        $table->dropColumn('last_name_1');
+        $table->dropColumn('last_name_2');
+        $table->dropColumn('gender_id');
+        $table->dropColumn('status');
+        });
+    }
+}
