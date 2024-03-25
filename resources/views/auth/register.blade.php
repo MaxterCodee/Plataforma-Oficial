ead0763a5072291bc937eb7d91eeae51a1bed009
Div3r <udhgjkf@gmail.com>
2024-03-24 22:50:59 -0600

    Login xd

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,143 +7,112 @@
         <div class="card-body">
             <form method="POST" action="{{ route('register') }}">
                 @csrf
-                {{-- Name --}}
-                <div class="row mb-3">
-                    <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+                <div class="container mt-5">
+        <form method="POST" action="{{ route('register') }}">
+            @csrf
 
-                    <div class="col-md-6">
-                        <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                        @error('name')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
+            <div class="row g-3">
+                <!-- Name -->
+                <div class="col-md-6">
+                    <label for="name" class="form-label">{{ __('Nombre') }}</label>
+                    <input id="name" class="form-control" type="text" name="name" value="{{ old('name') }}" required autofocus autocomplete="name" />
+                    @error('name')
+                        <p {{ $attributes->merge(['class' => 'text-sm text-red-600']) }}>{{ $message }}</p>
+                    @enderror
                 </div>
 
-                {{-- Last name 1 --}}
-                <div class="row mb-3">
-                    <label for="last_name_1" class="col-md-4 col-form-label text-md-end">{{ __('Last Name 1') }}</label>
+                <!-- Last Name 1 -->
+                <div class="col-md-6">
+                    <label for="last_name_1" class="form-label">{{ __('Apellido Materno') }}</label>
+                    <input id="last_name_1" class="form-control" type="text" name="last_name_1" value="{{ old('last_name_1') }}" required autofocus autocomplete="name" />
 
-                    <div class="col-md-6">
-                        <input id="last_name_1" type="text" class="form-control" name="last_name_1" :value="{{ old('last_name_1') }}" required autocomplete="last_name_1" autofocus>
-
-                        @error('name')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
+                    @error('last_name_1')
+                        <p {{ $attributes->merge(['class' => 'text-sm text-red-600']) }}>{{ $message }}</p>
+                    @enderror
                 </div>
 
-                {{-- Last name 2 --}}
-                <div class="row mb-3">
-                    <label for="last_name_2" class="col-md-4 col-form-label text-md-end">{{ __('Last Name 2') }}</label>
-
-                    <div class="col-md-6">
-                        <input id="last_name_2" type="text" class="form-control" name="last_name_2" :value="{{ old('last_name_2') }}" required autocomplete="last_name_2" autofocus>
-
-                        @error('name')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
+                <!-- Last Name 2 -->
+                <div class="col-md-6">
+                    <label for="last_name_2" class="form-label">{{ __('Apellido Paterno') }}</label>
+                    <input id="last_name_2" class="form-control" type="text" name="last_name_2"  required autocomplete="last_name_2" />
+                    @error('last_name_2')
+                        <p {{ $attributes->merge(['class' => 'text-sm text-red-600']) }}>{{ $message }}</p>
+                    @enderror
                 </div>
 
-                {{-- Email --}}
-                <div class="row mb-3">
-                    <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                    <div class="col-md-6">
-                        <input id="email" type="email" class="form-control" name="email" :value="{{ old('email') }}" required autocomplete="email">
-
-                        @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
+                <!-- Email Address -->
+                <div class="col-md-6">
+                    <label for="email" class="form-label">{{ __('Correo Electrónico') }}</label>
+                    <input id="email" class="form-control" type="email" name="email"  required autocomplete="username" />
+                    @error('email')
+                        <p {{ $attributes->merge(['class' => 'text-sm text-red-600']) }}>{{ $message }}</p>
+                    @enderror
                 </div>
 
-                {{-- Genders --}}
-                <div class="row mb-3">
-                    <label for="gender_id" class="col-md-4 col-form-label text-md-end">{{ __('Gender') }}</label>
-
-                    <div class="col-md-6">
-                        <select id="gender_id"  class="form-select form-control border border-gray-300 rounded-lg" name="gender_id" :value="{{ old('gender_id') }}" required>
-                            <option selected>{{__('Select Gender')}}</option>
-                            @foreach ($genders as $gender)
-                                <option value="{{ $gender->id }}">{{ $gender->name }}</option>
-                            @endforeach
-                        </select>
-                    </div>
+                <!-- Gender ID -->
+                <div class="col-md-6">
+                    <label for="gender_id" class="form-label">{{ __('Género') }}</label>
+                    <select id="gender_id" name="gender_id" class="form-select" required>
+                        <option selected>Seleccionar género</option>
+                        @foreach ($genders as $gender)
+                            <option value="{{ $gender->id }}">{{ $gender->name }}</option>
+                        @endforeach
+                    </select>
+                    @error('gender_id')
+                        <p {{ $attributes->merge(['class' => 'text-sm text-red-600']) }}>{{ $message }}</p>
+                    @enderror
                 </div>
 
-                {{-- Student Code (Matricula)--}}
-                <div class="row mb-3">
-                    <label for="matricula" class="col-md-4 col-form-label text-md-end">{{ __('Student Code') }}</label>
-
-                    <div class="col-md-6">
-                        <input id="matricula" type="text" class="form-control " name="matricula" :value="{{ old('matricula') }}" required autocomplete="matricula">
-
-                        @error('matricula')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
+                <!-- Matricula -->
+                <div class="col-md-6">
+                    <label for="matricula" class="form-label">{{ __('Matrícula') }}</label>
+                    <input id="matricula" class="form-control" type="text" name="matricula" required autocomplete="matricula" />
+                    @error('matricula')
+                        <p {{ $attributes->merge(['class' => 'text-sm text-red-600']) }}>{{ $message }}</p>
+                    @enderror
                 </div>
 
-                {{-- Career ID --}}
-                <div class="row mb-3">
-                    <label for="career_id" class="col-md-4 col-form-label text-md-end">{{ __('Career') }}</label>
-
-                    <div class="col-md-6">
-                        <select id="career_id"  class="form-select form-control border border-gray-300 rounded-lg" name="career_id" :value="{{ old('career_id') }}" required>
-                            <option selected>{{__('Select Career')}}</option>
-                            @foreach ($careers as $career)
-                                <option value="{{ $career->id }}">{{ $career->name }}</option>
-                            @endforeach
-                        </select>
-                    </div>
+                <!-- Career ID -->
+                <div class="col-md-6">
+                    <label for="career_id" class="form-label">{{ __('Carrera') }}</label>
+                    <select id="career_id" name="career_id" class="form-select" style="width: 192px;" required>
+                        <option selected>Seleccionar carrera</option>
+                        @foreach ($careers as $career)
+                            <option value="{{ $career->id }}">{{ $career->name }}</option>
+                        @endforeach
+                    </select>
+                    @error('career_id')
+                        <p {{ $attributes->merge(['class' => 'text-sm text-red-600']) }}>{{ $message }}</p>
+                    @enderror
                 </div>
 
-                {{-- Password --}}
-                <div class="row mb-3">
-                    <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                    <div class="col-md-6">
-                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                        @error('password')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
+                <!-- Password -->
+                <div class="col-md-6">
+                    <label for="password" class="form-label">{{ __('Contraseña') }}</label>
+                    <input id="password" class="form-control" type="password" name="password" required autocomplete="new-password" />
+                    @error('career_id')
+                        <p {{ $password->merge(['class' => 'text-sm text-red-600']) }}>{{ $message }}</p>
+                    @enderror
                 </div>
 
-                {{-- Confirm Password --}}
-                <div class="row mb-3">
-                    <label for="password-confirmation" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                    <div class="col-md-6">
-                        <input id="password-confirmation" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                    </div>
+                <!-- Confirm Password -->
+                <div class="col-md-6">
+                    <label for="password_confirmation" class="form-label">{{ __('Confirmar Contraseña') }}</label>
+                    <input id="password_confirmation" class="form-control" type="password" name="password_confirmation" required autocomplete="new-password" />
+                    @error('password_confirmation')
+                        <p {{ $password->merge(['class' => 'text-sm text-red-600']) }}>{{ $message }}</p>
+                    @enderror
                 </div>
 
-                <a class="btn btn-link" href="{{url('/')}}">
-                    {{__('Inicia sesion')}}
-                </a>
+                <input id="status" class="form-control invisible" type="text" name="status" value="1" required autocomplete="status" />
 
-                <br>
-
-                <button type="submit" class="btn btn-primary">
-                    {{ __('Register') }}
-                </button>
-                
+                <div class="col-12 mt-4">
+                    <a class="text-decoration-none me-4" href="{{ route('login') }}">{{ __('Already registered?') }}</a>
+                    <button type="submit" class="btn btn-primary">{{ __('Register') }}</button>
+                </div>
+            </div>
+        </form>
+    </div>
             </form>
         </div>
     </div>
